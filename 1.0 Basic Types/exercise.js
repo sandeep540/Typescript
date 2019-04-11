ba73be533db858f184d700dbe4a017b789c6327f
Sandeep <kn.sandeep@gmail.com>
2019-04-11 13:40:59 -0500

    updated

---
+++ 1.0 Basic Types/exercise.js
@@ -0,0 +1,18 @@
+var isDone = false;
+var decimal = 6;
+var color = "blue";
+var fullName = "Bob Bobbington";
+var list = [1, 2, 3];
+var x = ["sandeep", 100];
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+var notSure = 4;
+var bag = [1, true, "free"];
+var unusable = undefined;
+var n = null;
---
+++ 1.0 Basic Types/exercise.ts
@@ -0,0 +1,20 @@
+let isDone: boolean = false;
+
+let decimal: number = 6;
+
+let color: string = "blue";
+let fullName: string = `Bob Bobbington`;
+
+let list: Array<number> = [1, 2, 3];
+
+let x: [string, number] = ["sandeep", 100];
+
+enum Color {Red, Green, Blue};
+let c: Color = Color.Green;
+
+
+let notSure: any = 4;
+let bag: any[] = [1, true, "free"];
+
+let unusable: void = undefined;
+let n: null = null;
